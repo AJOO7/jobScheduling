869406b0be46553cc3c3e7e2c28b5b350e7c1de5
AJOO7 <67185443+AJOO7@users.noreply.github.com>
2021-11-17 08:59:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //begining 
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.PORT;
 
 app.use(express.urlencoded());
 const path = require('path');
